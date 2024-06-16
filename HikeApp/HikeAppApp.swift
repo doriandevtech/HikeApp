2aba06bc7053b1fe519510d8723bb34c06987715
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-06-16 22:18:21 +0200

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Dorian Emenir on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
