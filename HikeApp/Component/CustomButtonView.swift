9b6993285d8f6ee4b3b85ff81ef62d706474e975
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-06-23 22:57:42 +0200

    Create: custom button with LineardGradients defined colors and a systemName image as logo 👨🏻‍🎨

--- HikeApp/Component/CustomButtonView.swift
+++ HikeApp/Component/CustomButtonView.swift
@@ -1,5 +1,5 @@
 //
-//  CutomButtonView.swift
+//  CustomButtonView.swift
 //  HikeApp
 //
 //  Created by Dorian Emenir on 23/06/2024.
@@ -7,12 +7,55 @@
 
 import SwiftUI
 
-struct CutomButtonView: View {
+struct CustomButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .white,
+                            .colorGreenLight,
+                            .colorGreenMedium
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+            
+            Circle()
+                .stroke(
+                    LinearGradient(
+                        colors: [
+                            .colorGrayLight,
+                            .colorGrayMedium
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    ),
+                    lineWidth: 4
+                )
+            
+            Image(systemName: "figure.hiking")
+                .fontWeight(.black)
+                .font(.system(size: 30))
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [
+                            .colorGrayLight,
+                            .colorGrayMedium
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+        } //: ZSTACK
+        .frame(width: 58, height: 58)
     }
 }
 
 #Preview {
-    CutomButtonView()
+    CustomButtonView()
+        .previewLayout(.sizeThatFits)
+        .padding()
 }
