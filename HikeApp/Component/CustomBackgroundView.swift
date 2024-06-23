b3e41c3a90e8287375d39200075bf0c1d71e69d3
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-06-23 22:12:23 +0200

    Create: CustomBackgroundView & ColorExtension files 🗂️

---
+++ HikeApp/Component/CustomBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomBackgroundView.swift
+//  HikeApp
+//
+//  Created by Dorian Emenir on 23/06/2024.
+//
+
+import SwiftUI
+
+struct CustomBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomBackgroundView()
+}
---
+++ HikeApp/Utility/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  HikeApp
+//
+//  Created by Dorian Emenir on 23/06/2024.
+//
+
+import Foundation
