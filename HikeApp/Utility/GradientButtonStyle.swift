d7741065acea6c71f43959c8306d7f8082e8a170
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-06-23 23:49:38 +0200

    Create: GradientButton struct in order to customize a given button's style 👨🏻‍🎨

--- HikeApp/Utility/GradientButtonStyle.swift
+++ HikeApp/Utility/GradientButtonStyle.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                // Conditional Statement with Nul Coalescing
+                // Condition ? A : B
+                configuration.isPressed ?
+                // A: When user press the button
+                LinearGradient(
+                    colors: [
+                        .colorGrayMedium,
+                        .colorGrayLight
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                
+                :
+                
+                // B: When the Button is not pressed
+                LinearGradient(
+                    colors: [
+                        .colorGrayLight,
+                        .colorGrayMedium
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+            .clipShape(.rect(cornerRadius: 40))
+    }
+}
