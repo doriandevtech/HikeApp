ce0ccaaeb9c61ebc20fb8d7f395c0b1c55dd55c6
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-06-23 23:48:52 +0200

    Update: add a "Explore More" button & a random image generator to change the displayed image when clicked 👷🏻‍♂️

--- HikeApp/Component/CardView.swift
+++ HikeApp/Component/CardView.swift
@@ -8,6 +8,32 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    
+    func randomImage() {
+        print("--- BUTTON WAS PRESSED ---")
+        print("Status: Old Image Number = \(imageNumber)")
+        
+        // Generate a random number until the the generated number is different than the previous one
+        // This avoids having the same image displayed multiple times in a row
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+            print("Action: Random Number Generated = \(randomNumber)")
+            
+        } while randomNumber == imageNumber
+        
+        imageNumber = randomNumber
+        
+        print("Result: New Image Number = \(imageNumber)")
+        print("--- THE END ---")
+        print("\n")
+    }
+    
     var body: some View {
         // MARK: - CARD
         
@@ -34,7 +60,7 @@ struct CardView: View {
                             )
                         
                         Spacer()
-                    
+                        
                         Button {
                             // ACTION: Show a sheet
                             print("the button was pressed.")
@@ -43,7 +69,7 @@ struct CardView: View {
                         }
                         
                     }
-                
+                    
                     Text("Fun and enjoyable activity for friends and families.")
                         .multilineTextAlignment(.leading)
                         .italic()
@@ -67,12 +93,35 @@ struct CardView: View {
                         )
                         .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
-                    .scaledToFit()
+                        .scaledToFit()
+                        .animation(.default, value: imageNumber)
                 }
                 
                 // MARK: - FOOTER
+                
+                Button {
+                    // ACTION: Generate a random number
+                    print("Hello")
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .colorGreenLight,
+                                    .colorGreenMedium
+                                ],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .shadow(color: .black.opacity(0.25), radius:  0.25, x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
             }
         } //: CARD
         .frame(width: 320, height: 570)
