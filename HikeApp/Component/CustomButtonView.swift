46b0cebb3f58c51b75f3921454c677414d7aefb9
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-06-23 22:55:46 +0200

    Create: CustomButtonView file 🗂️

---
+++ HikeApp/Component/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CutomButtonView.swift
+//  HikeApp
+//
+//  Created by Dorian Emenir on 23/06/2024.
+//
+
+import SwiftUI
+
+struct CutomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CutomButtonView()
+}
