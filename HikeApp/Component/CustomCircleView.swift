fb9da9209a09d0dc67a6ee0dfbd7c3cfbaeb20a8
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-06-26 22:58:15 +0200

    Create: CustomCircleView & MotionAnimationView files 🗂️

---
+++ HikeApp/Component/CustomCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCircleView.swift
+//  HikeApp
+//
+//  Created by Dorian Emenir on 26/06/2024.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
---
+++ HikeApp/Component/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  HikeApp
+//
+//  Created by Dorian Emenir on 26/06/2024.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
