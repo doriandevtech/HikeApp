9a58d5d91d47046dbd74c70a12d201f35909153e
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-07-02 21:04:35 +0200

    Update: MotionAnimationView with multiple steps to create randomly generated circles 👷🏻‍♂️

--- HikeApp/Component/MotionAnimationView.swift
+++ HikeApp/Component/MotionAnimationView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct MotionAnimationView: View {
+    // MARK: PROPERTIES
+    
+    @State private var randomCircle: Int = Int.random(in: 6...12)
+    
+    // MARK: FUNCTIONS
+    
+    // 1. RANDOM COORDINATE
+    func randomCoordinate() -> CGFloat {
+        return CGFloat.random(in: 0...256)
+    }
+    
+    // 2. RANDOM SIZE
+    
+    // 3. RANDOM SCALE
+    
+    // 4. RANDOM SPEED
+    
+    // 5. RANDOM DELAY
+    
+    
     var body: some View {
         ZStack {
-            Circle()
-                .foregroundStyle(.white)
+            ForEach(0...randomCircle, id: \.self) { item in
+                Circle()
+                    .foregroundStyle(.white)
+                    .opacity(0.25)
+                    .position(
+                        x: randomCoordinate(),
+                        y: randomCoordinate()
+                    )
+            }
         } //: ZSTACK
         .frame(width: 256, height: 256)
     }
