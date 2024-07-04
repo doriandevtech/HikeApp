359dec3f5e0b61794ecfa3b333a1ff087b64632a
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-07-04 18:52:10 +0200

    Create: new SettingsView file 🗂️

---
+++ HikeApp/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  HikeApp
+//
+//  Created by Dorian Emenir on 04/07/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
