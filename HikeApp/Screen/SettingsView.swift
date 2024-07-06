05a2eadfcf321b4b9c91290240571d2f75fdca08
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-07-06 22:13:41 +0200

    Create: Basic labelled content 👨🏻‍🎨

--- HikeApp/Screen/SettingsView.swift
+++ HikeApp/Screen/SettingsView.swift
@@ -78,7 +78,8 @@ struct SettingsView: View {
                 }
                     .padding(.vertical, 8)
             ) {
-                Text("List Row")
+                // 1. Basic Labeled Content
+                LabeledContent("Application", value: "Hike")
             } //: SECTION
             
         }
